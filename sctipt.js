f77c344e0dc15f3786c580ecff6a89baab2a3992
red wolf <myash7535@gmail.com>
2024-08-04 00:38:03 +0530

    completed

--- sctipt.js
+++ sctipt.js
@@ -15,7 +15,17 @@ let sound = {
     't':new Audio('./yoimo.mp3'),
     'y':new Audio('./ara.mp3'),
     'u': new Audio('./wow.mp3'),
-    'i': new Audio('./puneet.mp3')
+    'i': new Audio('./puneet.mp3'),
+    'o': new Audio('./gta.mp3'),
+    'z': new Audio('./among-us-role-reveal-sound.mp3'),
+    'b': new Audio('./ny-video-online-audio-converter.mp3'),
+    '?':new Audio('./bengali-cuss-words-made-with-Voicemod.mp3'),
+    'x': new Audio('./hacker-hai-bhai-hacker-ajjubhai.mp3'),
+    'm':new Audio('./maqsad-music.mp3'),
+    'c':new Audio('./ahh-qe4dcns7.mp3'),
+    "+":new Audio('./ye-le-teri-bread.mp3'),
+    'v':new Audio('./suiiiiiiiiiii.mp3'),
+    'n':new Audio('./sequence-01_zf9vGFr.mp3')
 };
 
 document.addEventListener('keypress', (e) => {
